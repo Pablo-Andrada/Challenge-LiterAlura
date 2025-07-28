968ddd1b0811b2196bfe170970c93e74bf7f07f8
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-07-28 01:13:19 -0300

    First commit

---
+++ src/main/java/com/aluracursos/chellenge/ChellengeLiterAluraApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.chellenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChellengeLiterAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChellengeLiterAluraApplication.class, args);
+	}
+
+}
